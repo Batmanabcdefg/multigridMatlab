be4bddb3cd3e228c43435b296bcc8e23a8030be9
raymondt8 <raymond.toft@gmail.com>
2017-03-31 11:20:35 +0200

    Removed boundary bug in newtonTwoPhaseADV2.m

--- newtonTwoPhaseADV2.m
+++ newtonTwoPhaseADV2.m
@@ -37,30 +37,30 @@ function [p_ad, sW_ad,nit] =  ...
    nit = 0;
    resNorm = 1e99;
    old_res = resNorm;
-   
+   boundaryCondition =0;
   while (resNorm > tol) && (nit < maxits) && (old_res >= resNorm)
       old_res = resNorm;
      
       [water, oil] = computePhaseFlux(model,p_ad,sW_ad,dt,g,p0,sW0);
 
-      if(isempty(varargin) || (length(varargin) ~= 3) || nit > 0)
+      if(isempty(varargin) || (length(varargin) ~= 3))% || nit > 0)
         [water, oil] = computeBoundaryCondition(model,p_ad,sW_ad,water,oil);
       
       else
         boundaryCondition = varargin{3};
-        water = boundaryCondition.water;
-        oil = boundaryCondition.oil;
+        water_boundary = boundaryCondition.water;
+        oil_boundary = boundaryCondition.oil;
         [water, oil] = initVariablesADI(water.val,oil.val);
         %Ensure the ADI jacobi entries for the prodiction well are set
         %properly - assume this needs to be done
-        water_val = water(model.well.prodIndex).val;
+        water_val = water_boundary(model.well.prodIndex).val;
        
         water(model.well.prodIndex) = water(model.well.prodIndex) - water(model.well.prodIndex) ...
-            + p_ad(model.well.prodIndex) - p_ad(model.well.prodIndex).val + water_val;
+            + p_ad(model.well.prodIndex) - p_ad(model.well.prodIndex).val - water_val;
       
-        oil_val = oil(model.well.prodIndex).val;
+        oil_val = oil_boundary(model.well.prodIndex).val;
         oil(model.well.prodIndex) = oil(model.well.prodIndex) - oil(model.well.prodIndex) ...
-            + sW_ad(model.well.prodIndex)- sW_ad(model.well.prodIndex).val + oil_val;
+            + sW_ad(model.well.prodIndex)- sW_ad(model.well.prodIndex).val - oil_val;
   
       end
       
