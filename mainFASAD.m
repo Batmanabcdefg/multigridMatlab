5d270e74299fe18bb0618fe5465bcc9176b8558b
raymondt8 <raymond.toft@gmail.com>
2017-05-12 13:04:02 +0200

    Added testcase 1a to 7b

--- mainFASAD.m
+++ mainFASAD.m
@@ -11,9 +11,50 @@ grid = computeGeometry(grid);
 
 plotGrid(grid); view(3); axis tight
 
+% Set rock properties
+  homogeneous = 'true';
+  % (Outdated) value ranges
+  % permeability range: {poor: 1-15, moderate: 15-20, good: 50-250, very
+  % good: 250-1000
+  % porosity range: {fair: 0.25, very low: 0.1}
+  perm = 30*milli*darcy; 
+  poro = 0.25;
 
+  perm_range = [0.1 0.4];
+  gauss_filter_size = [3 3 3];
+  std = 2.5;
+  % Compressibility: normally in the range of 10^-6 to 10^-7, assumed to be
+  % constant
+  cr   = 1e-6/barsa;
+  % Reference pressure
+  p_r  = 200*barsa;
+ 
+% Set fluid properties
+  % Water
+  muW    = 1*centi*poise;
+  cw     = 1e-5/barsa;
+  rho_rw = 960*kilogram/meter^3;
+  rhoWS  = 1000*kilogram/meter^3;
+  
+  % Define a lighter, more viscous oil phase with different relative
+  % permeability function
+  muO   = 5*centi*poise;
+  % Compressibility range: {slighly: 10^-5 to 10^-6, compressible: 10^-3 to
+  % 10^-4}psi^-1
+  co      = 1e-3/barsa; %1e-4
+  rho_ro = 1050*kilogram/meter^3; % 850
+  rhoOS  = 750*kilogram/meter^3; % 750
+    
+  newModel = struct('grid',grid, 'muW',muW, 'cw',cw, 'rho_rw',rho_rw,  ...
+                    'rhoWS',rhoWS, 'muO', muO, 'co',co, 'rho_ro',rho_ro, ...
+                    'rhoOS', rhoOS, 'cr',cr,'p_r',p_r, 'poro',poro,'perm', perm, ...
+                    'homogeneous',homogeneous, 'perm_range',perm_range, ... 
+                    'gauss_filter_size',gauss_filter_size, 'std',std);
+  mode = 'newModel';
+  initMode = struct('mode', mode, 'model',newModel);
+  
 % Initiate complete model
-model = initiateModel(grid);
+model = initiateModel(initMode);
 
 % 
 % p = linspace(100*barsa,220*barsa,50)';
